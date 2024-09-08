706795b5d28f5e697bcbd694bd0a48c45859e559
Hoang1901 <nguyentranhoang619@gmail.com>
2024-09-08 14:05:17 +0700

    push2

--- C# Basic/NewApp/Program.cs
+++ C# Basic/NewApp/Program.cs
@@ -1,35 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-// Console.WriteLine("I'm NguyenTranHoang_2021050282!");
-// Console.Write("6");
-// int c = 9;
-// Console.WriteLine("a = " + c);
-// int a = 9;
-// int b = 3;
-// Console.WriteLine("{0}+{1}={2}",a,b,a + b);
-internal class First
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        // Console.Write("Nhap vao du lieu: ");
-        // int a = Console.Read();
-        // Console.WriteLine("Du lieu vua nhap vao la: "+ a);
-
-        Console.Write("Nhap vao du lieu: ");
-        string str = Console.ReadLine();
-        Console.WriteLine("Du lieu vua nhap vao la: "+ str);
-
-        //khai bao bien kieu string va gan gia tri = Nguyen Van A
-        const string hoten = "Nguyen Van A";
-        //khai bao bien kieu int va gan gia tri = 18
-        int tuoi = 18;
-        //khai bao hang kieu int 
-        const int namlamviec = 8;
-        Console.WriteLine("Sinh vien {0} - {1} tuoi - {2} nam lam viec!",hoten,tuoi,namlamviec);
-
-        string str1 = "123";
-        int a = int.Parse(str1);
-        System.Console.WriteLine("a = " + a);
-        
+        string str = "123";  //gia tri dau vao can chuyen doi
+        int ketQua;  //bien luu ket qua sau chuyen doi
+        //bien kiem tra chuyen doi co thanh cong hay khong
+        bool kiemTra = false;
+        //chuyen doi kieu  du lieu
+        kiemTra = int.TryParse(str, out(ketQua));
+        System.Console.WriteLine("Ket qua = " + ketQua);
     }
 }
--- C# Basic/NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ C# Basic/NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fc2e3c5b0df3078747942f5174b606ceeebd15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d12bc1eb6191c49df2cee16ac2420e24b9896c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
