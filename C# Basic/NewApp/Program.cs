277b8302a0ca65bf686eb68059bfdca82b29d25a
Hoang1901 <nguyentranhoang619@gmail.com>
2024-09-12 01:36:42 +0700

    push10

--- C# Basic/NewApp/Program.cs
+++ C# Basic/NewApp/Program.cs
@@ -5,16 +5,8 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        int day = 2;
-        switch (day){
-            case 1: System.Console.WriteLine("Chu nhat"); break;
-            case 2: System.Console.WriteLine("Thu hai"); break;
-            case 3: System.Console.WriteLine("Thu ba"); break;
-            case 4: System.Console.WriteLine("Thu tu"); break;
-            case 5: System.Console.WriteLine("Thu nam"); break;
-            case 6: System.Console.WriteLine("Thu sau"); break;
-            case 7: System.Console.WriteLine("Thu bay");break;
-            default: System.Console.WriteLine("Khong phai ngay trong tuan"); break;
+        for (int i = 0; i <=10; i++){
+            System.Console.WriteLine("Vong lap thu {0}",i);
         }
     }
 }
