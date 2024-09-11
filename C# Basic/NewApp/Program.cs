b8f203fa014353a947b8aaf99487118e4260918c
Hoang1901 <nguyentranhoang619@gmail.com>
2024-09-12 01:40:24 +0700

    push11

--- C# Basic/NewApp/Program.cs
+++ C# Basic/NewApp/Program.cs
@@ -5,8 +5,12 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        for (int i = 0; i <=10; i++){
-            System.Console.WriteLine("Vong lap thu {0}",i);
+        int a = 1;
+        while (a < 10){
+            System.Console.WriteLine("Vong lap thu {0}",a);
+            //tang a len 1 don vi de dam bao co luc a >= 10
+            //a >= 10 thi vong lap ket thuc
+            a++;
         }
     }
 }
--- C# Basic/NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ C# Basic/NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c662444fe723041978ebc52df22f53a9825df563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277b8302a0ca65bf686eb68059bfdca82b29d25a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
