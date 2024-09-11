9141e557d83ce5a411aee986f9093efd2bd893d5
Hoang1901 <nguyentranhoang619@gmail.com>
2024-09-12 01:15:40 +0700

    push7

--- C# Basic/NewApp/Program.cs
+++ C# Basic/NewApp/Program.cs
@@ -5,22 +5,17 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        int a, b; //khai bao 2 bien so a va b kieu int
-        // thong bao cho nguoi dung nhap vao a
-        System.Console.Write("a = ");
-        //doc du lieu nhap tu ban phim chuyen sang kieu int va gan cho a
-        a = Convert.ToInt32(Console.ReadLine());
-        System.Console.Write("b = ");
-        b = Convert.ToInt32(Console.ReadLine());
-        //in tong a+b ra ngoai man hinh
-        System.Console.WriteLine("{0} + {1} = {2}", a, b, a+b);
-        //in hieu a-b ra ngoai man hinh
-        System.Console.WriteLine("{0} - {1} = {2}", a, b, a-b);
-        //in tich a*b ra ngoai man hinh
-        System.Console.WriteLine("{0} * {1} = {2}", a, b, a*b);
-        System.Console.WriteLine("{0} / {1} = {2}", a, b, a/b);
-        System.Console.WriteLine("{0} % {1} = {2}", a, b, a%b);
-        //toan tu so sanh
-        System.Console.WriteLine("{0} > {1} = {2}", a, b, a>b);
+        float a  = 10;
+        if (a >= 8){
+            System.Console.WriteLine("Hoc sinh hoc luc gioi");
+        }else if(a >= 6.5){
+            System.Console.WriteLine("Hoc sinh hoc luc kha");
+        }else if(a >= 5){
+            System.Console.WriteLine("Hoc sinh hoc luc trung binh");
+        }else if(a >= 3.5){
+            System.Console.WriteLine("Hoc sinh hoc luc yeu");
+        }else {
+            System.Console.WriteLine("Hoc sinh hoc luc kem");
+        }
     }
 }
