b456d5a2b724a4718acff5f6c39337a26fad6111
Hoang1901 <nguyentranhoang619@gmail.com>
2024-09-12 02:17:31 +0700

    push13

--- C# Basic/NewApp/Program.cs
+++ C# Basic/NewApp/Program.cs
@@ -6,7 +6,7 @@ public class Program
     private static void Main(string[] args)
     {
         for (int i = 0; i <= 10; i++){
-            if (i == 5) break;
+            if (i == 5) continue;
                 System.Console.WriteLine("Vong lap thu {0}",i);
         }
         // int a = 1;
