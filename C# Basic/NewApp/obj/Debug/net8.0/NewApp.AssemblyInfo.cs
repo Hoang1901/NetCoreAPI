6a95445ba6725b4b512f0fa0e03e3d79e8bf9b78
Hoang1901 <nguyentranhoang619@gmail.com>
2024-09-12 01:24:41 +0700

    push8

--- C# Basic/NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ C# Basic/NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a782b0c21f0b8818667331b94a8f965f1528042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9141e557d83ce5a411aee986f9093efd2bd893d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
